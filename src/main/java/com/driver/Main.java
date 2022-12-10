40ad83e9a5e35d6c47936c0e775ca68f244b1879
AZMAT ALI <222azmat@gmail.com>
2022-12-10 18:58:31 +0530

    1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
+
 public class Main {
-  
+    static class A{
+        public static String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    static class B extends A{
+        public static String meth(){
+            return  "Method is overridden in Extendend class B";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(B.meth());
+    }
 }
